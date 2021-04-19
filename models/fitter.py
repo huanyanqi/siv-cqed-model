7d1d50c7a05f6a82122748d66e8b16c06966668b
huanyanqi <36173574+huanyanqi@users.noreply.github.com>
2021-04-19 21:26:09 +0800

    Shift to new method of fitting using lmfit Model class

--- models/fitter.py
+++ models/fitter.py
@@ -6,61 +6,37 @@ import lmfit
 
 from sivqed.models.cavity import Cavity, MultiQubitCavity
 
-# w, w_c, k_in, k_tot, A, B
-def cavity_ref(params, w, spectrum):
-    params = params.valuesdict()
+def cavity_ref(w, w_c, k_in, k_tot, A, B):
+    return A * Cavity.reflectance_fn(
+                        w, -1, 0, 0, 0,
+                        0, 0, 0,
+                        w_c, k_in, k_tot
+                        ) + B
 
-    return params["A"] * Cavity.reflectance_fn(
-                        w, -1, 0, 0, 0, 
-                        0, 0, 0, 
-                        params["w_c"], params["k_in"], params["k_tot"]
-                        ) + params["B"] - spectrum
+def single_siv_ref(w, w_up, g_up, gamma_up, w_c, k_in, k_tot, A, B):
+    return A * Cavity.reflectance_fn(
+                w, 1, 0, 0, 0,
+                w_up, g_up, gamma_up,
+                w_c, k_in, k_tot
+                ) + B
 
-# w, w_up, g_up, gamma_up, w_c, k_in, k_tot, A, B 
-def single_siv_ref(params, w, spectrum):
-    params = params.valuesdict()
+def two_siv_ref(w, w_up_1, g_up_1, gamma_up_1, w_up_2, g_up_2, gamma_up_2, w_c, k_in, k_tot, A, B):
 
-    return params["A"] * Cavity.reflectance_fn(
-                w, 1, 0, 0, 0, 
-                params["w_up"], params["g_up"], params["gamma_up"], 
-                params["w_c"], params["k_in"], params["k_tot"]
-                ) + params["B"] - spectrum
+    return A * MultiQubitCavity.reflectance_fn(w, 1,
+        [{"w_up": w_up_1, "g_up": g_up_1, "gamma_up": gamma_up_1},
+         {"w_up": w_up_2, "g_up": g_up_2, "gamma_up": gamma_up_2}],
+        w_c, k_in, k_tot) + B
 
-# w, w_up_1, g_up_1, gamma_up_1, w_up_2, g_up_2, gamma_up_2, w_c, k_in, k_tot, A, B
-def two_siv_ref(params, w, spectrum):
-    params = params.valuesdict()
+def three_siv_ref(w, w_up_1, g_up_1, gamma_up_1, w_up_2, g_up_2, gamma_up_2,
+                  w_up_3, g_up_3, gamma_up_3, w_c, k_in, k_tot, A, B):
 
-    return params["A"] * MultiQubitCavity.reflectance_fn(w, 1, 
-        [{"w_up": params["w_up_1"], "g_up": params["g_up_1"], "gamma_up": params["gamma_up_1"]},
-         {"w_up": params["w_up_2"], "g_up": params["g_up_2"], "gamma_up": params["gamma_up_2"]}], 
-        params["w_c"], params["k_in"], params["k_tot"]) + params["B"] - spectrum
+    return A * MultiQubitCavity.reflectance_fn(w, 1,
+        [{"w_up": w_up_1, "g_up": g_up_1, "gamma_up": gamma_up_1},
+         {"w_up": w_up_2, "g_up": g_up_2, "gamma_up": gamma_up_2},
+         {"w_up": w_up_3, "g_up": g_up_3, "gamma_up": gamma_up_3}],
+        w_c, k_in, k_tot) + B
 
-# w, w_up_1, g_up_1, gamma_up_1, w_up_2, g_up_2, gamma_up_2,
-# w_up_3, g_up_3, gamma_up_3, w_c, k_in, k_tot, A, B
-def three_siv_ref(params, w, spectrum):
-    params = params.valuesdict()
-
-    return params["A"] * MultiQubitCavity.reflectance_fn(w, 1, 
-        [{"w_up": params["w_up_1"], "g_up": params["g_up_1"], "gamma_up": params["gamma_up_1"]},
-         {"w_up": params["w_up_2"], "g_up": params["g_up_2"], "gamma_up": params["gamma_up_2"]},
-         {"w_up": params["w_up_3"], "g_up": params["g_up_3"], "gamma_up": params["gamma_up_3"]}], 
-        params["w_c"], params["k_in"], params["k_tot"]) + params["B"] - spectrum
-
-def fit_reflection(params, freqs, spectrum, fit_func, method='leastsq'):
-    """ Fit the reflection spectrum as a function of the laser frequency sweep (freqs). 
-    Returns the fitted parameters. 
-
-    params : Parameters object
-        Stores the list of parameters and associated bounds
-    freqs : Numpy array
-        List of frequencies (x-axis) to be fitted
-    spectrum : Numpy array
-        List of counts (y-axis) to be fitted
-    fit_func : Function
-        Function to be used for fitting
-    """
-
-    result = lmfit.minimize(fit_func, params, args=(freqs, spectrum), method=method)
-    return result
-
-
+cavity_ref_model = lmfit.Model(cavity_ref)
+single_siv_ref_model = lmfit.Model(single_siv_ref)
+two_siv_ref_model = lmfit.Model(two_siv_ref)
+three_siv_ref_model = lmfit.Model(three_siv_ref)
